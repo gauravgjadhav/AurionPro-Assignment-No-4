fc33a1aa5a38d283d7c1ed910ceefbce2ac332fe
gauravgjadhav <92154289+gauravgjadhav@users.noreply.github.com>
2022-06-14 22:53:11 +0530

    Add files via upload

---
+++ ParmsArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+public class ParamArray
+{
+    public static void Main(string[] args)
+    {
+        UseParms(1, 2, 3, 4);
+        UseParms(1, 'a');
+        UseParms2();
+        int[] myIntArray = { 5, 6, 7, 8, 9 };
+        UseParms(myIntArray);
+
+        Object[] myObjectArray = { 2,'b',"test","again" };
+        UseParms2(myObjectArray);
+        UseParms2(myIntArray);
+
+    }
+
+    public static void UseParms(params int[] list)
+    {
+        for(int i=0; i < list.Length; i++)
+        {
+            Console.Write(list[i]+"  ");
+        }
+        Console.WriteLine();
+    }
+    public static void UseParms2(params object[] list)
+    {
+        for(int i=0; i < list.Length; i++)
+        {
+            Console.Write(list[i]+" ");
+        }
+        Console.WriteLine();
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class TreyParseMethod
+{
+    public static void Main(string[] args)
+    {
+        bool res1;
+        int a;
+        string str = "12";
+        res1 = int.TryParse(str, out a);
+        Console.WriteLine("String is a numeric representation :" + res1);
+    }
+}
