60e478fe8bd7f6d2bdb9071a399d1c289ca8daef
gauravgjadhav <92154289+gauravgjadhav@users.noreply.github.com>
2022-06-14 22:42:37 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class InAndOutModifier
+{
+    public static void Main(string[] args)
+    {
+        int a = 44;
+        InArrayExample(a);
+        Console.WriteLine(a);
+
+       // int a;
+        OutArgExample(out a);
+        Console.WriteLine(a);
+    }
+    static void InArrayExample(in int number)
+    {
+        //int number = 10;it give a error it is ReadOnly veriable
+    }
+    static void OutArgExample(out int number)
+    {
+        number = 10;
+    }
+}
