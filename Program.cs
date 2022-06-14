fdb6279020e771201b2e2fe1c4a23917a2158c30
gauravgjadhav <92154289+gauravgjadhav@users.noreply.github.com>
2022-06-14 22:45:14 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
-class TreyParseMethod
+public class ParamArray
 {
     public static void Main(string[] args)
     {
-        bool res1;
-        int a;
-        string str = "12";
-        res1 = int.TryParse(str, out a);
-        Console.WriteLine("String is a numeric representation :" + res1);
+        UseParms(1, 2, 3, 4);
+        UseParms(1, 'a');
+        UseParms2();
+        int[] myIntArray = { 5, 6, 7, 8, 9 };
+        UseParms(myIntArray);
+
+        Object[] myObjectArray = { 2,'b',"test","again" };
+        UseParms2(myObjectArray);
+        UseParms2(myIntArray);
+
     }
+
+    public static void UseParms(params int[] list)
+    {
+        for(int i=0; i < list.Length; i++)
+        {
+            Console.Write(list[i]+"  ");
+        }
+        Console.WriteLine();
+    }
+    public static void UseParms2(params object[] list)
+    {
+        for(int i=0; i < list.Length; i++)
+        {
+            Console.Write(list[i]+" ");
+        }
+        Console.WriteLine();
+    }
+
 }
